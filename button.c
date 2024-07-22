34a3943609e2e20e9c714ec1aeaa5bbfe5cc1f32
Adrian Pietrzak <adrianp126p@gmail.com>
2024-07-22 13:05:26 +0200

    First commit

---
+++ button.c
@@ -0,0 +1,160 @@
+/*
+ * button.c
+ *
+ *  Created on: May 7, 2022
+ *      Author: Adrian
+ */
+
+#include "main.h"
+#include "button.h"
+
+
+
+//Button init
+void ButtonInitKey(button_t * Key, GPIO_TypeDef *GpioPort, uint16_t GpioPin, uint32_t TimerDebounce,
+			uint32_t TimerLongPressed, uint32_t TimerRepeat, ReverseLogicGpio_t ReverseLogic, uint16_t Number)
+{
+	Key->State = IDLE;
+	Key->GpioPort = GpioPort;
+	Key->GpioPin = GpioPin;
+	Key->TimerDebounce = TimerDebounce;
+	Key->TimerLongPressed = TimerLongPressed;
+	Key->TimerRepeat = TimerRepeat;
+
+	Key->ReverseLogic = ReverseLogic;
+	Key->NumberBtn = Number;
+}
+//Time settings function
+void ButtonSetDebounceTime(button_t * Key, uint32_t Miliseconds)
+{
+	Key->TimerDebounce = Miliseconds;
+}
+
+void ButtonSetLongPressedTime(button_t *Key, uint32_t Miliseconds)
+{
+	Key->TimerLongPressed = Miliseconds;
+}
+
+void ButtonSetRepeatTime(button_t *Key, uint32_t Miliseconds)
+{
+	Key->TimerRepeat = Miliseconds;
+}
+
+//Callbacks
+void ButtonRegisterPressCallback(button_t *Key, void *Callback)
+{
+	Key->ButtonPressed = Callback;
+}
+
+void ButtonRegisterLongPressedCallback(button_t *Key, void *Callback)
+{
+	Key->ButtonLongPressed = Callback;
+}
+
+void ButtonRegisterRepeatCallback(button_t *Key, void *Callback)
+{
+	Key->ButtonRepeat = Callback;
+}
+
+void ButtonRegisterReleaseCallback(button_t *Key, void *Callback)
+{
+	Key->ButtonRelease = Callback;
+}
+//States routine
+void ButtonIdleRoutine(button_t *Key)
+{
+	if(HAL_GPIO_ReadPin(Key->GpioPort, Key->GpioPin) == (Key->ReverseLogic==0)?GPIO_PIN_RESET:GPIO_PIN_SET)
+	{
+		Key->LastTick = HAL_GetTick();
+		Key->State = DEBOUNCE;
+	}
+}
+
+void ButtonDebounceRoutine(button_t *Key)
+{
+	if((HAL_GetTick() - Key->LastTick) >= Key->TimerDebounce)
+	{
+		if(HAL_GPIO_ReadPin(Key->GpioPort, Key->GpioPin) == (Key->ReverseLogic==0)?GPIO_PIN_RESET:GPIO_PIN_SET)
+		{
+			Key->State = PRESSED;
+			Key->LastTick = HAL_GetTick();
+			if(Key->ButtonPressed != NULL)
+			{
+				Key->ButtonPressed(Key->NumberBtn);
+			}
+		}
+		else
+		{
+			Key->State = IDLE;
+		}
+	}
+}
+
+void ButtonPressedRoutine(button_t *Key)
+{
+	if(HAL_GPIO_ReadPin(Key->GpioPort, Key->GpioPin) == (Key->ReverseLogic==0)?GPIO_PIN_SET:GPIO_PIN_RESET)
+	{
+		Key->State = RELEASE;
+	}
+	else if(HAL_GetTick() - Key->LastTick >= Key->TimerLongPressed)
+	{
+		Key->State = REPEAT;
+		Key->LastTick = HAL_GetTick();
+		if(Key->ButtonLongPressed != NULL)
+		{
+			Key->ButtonLongPressed(Key->NumberBtn);
+		}
+	}
+}
+
+void ButtonRepeatRoutine(button_t *Key)
+{
+	if(HAL_GPIO_ReadPin(Key->GpioPort, Key->GpioPin) == (Key->ReverseLogic==0)?GPIO_PIN_SET:GPIO_PIN_RESET)
+	{
+		Key->State = RELEASE;
+	}
+	else if(HAL_GetTick() - Key->LastTick >= Key->TimerRepeat)
+	{
+		Key->LastTick = HAL_GetTick();
+		if(Key->ButtonRepeat != NULL)
+		{
+			Key->ButtonRepeat(Key->NumberBtn);
+		}
+	}
+}
+
+void ButtonReleaseRoutine(button_t *Key)
+{
+	if(Key->ButtonRelease != NULL)
+	{
+		Key->ButtonRelease(Key->NumberBtn);
+	}
+	Key->State = IDLE;
+}
+
+//State machines
+void ButtonTask(button_t *Key)
+{
+	switch(Key->State)
+	{
+	case IDLE:
+		ButtonIdleRoutine(Key);
+		break;
+
+	case DEBOUNCE:
+		ButtonDebounceRoutine(Key);
+		break;
+
+	case PRESSED:
+		ButtonPressedRoutine(Key);
+		break;
+
+	case REPEAT:
+		ButtonRepeatRoutine(Key);
+		break;
+
+	case RELEASE:
+		ButtonReleaseRoutine(Key);
+		break;
+	}
+}
---
+++ button.h
@@ -0,0 +1,65 @@
+/*
+ * button.h
+ *
+ *  Created on: May 7, 2022
+ *      Author: Adrian
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+// States of state machine
+
+typedef enum{
+	IDLE = 0,
+	DEBOUNCE,
+	PRESSED,
+	REPEAT,
+	RELEASE
+
+} BUTTON_STATE;
+
+typedef enum{
+	NON_REVERSE = 0,
+	REVERSE
+}ReverseLogicGpio_t;
+
+// Struct for buttons
+typedef struct
+{
+	BUTTON_STATE  State;
+
+	GPIO_TypeDef *GpioPort;
+	uint16_t      GpioPin;
+
+	uint32_t      LastTick;
+	uint32_t      TimerDebounce;
+	uint32_t	  TimerLongPressed;
+	uint32_t	  TimerRepeat;
+
+	uint8_t		  ReverseLogic;
+	uint16_t	  NumberBtn;
+
+	void(*ButtonPressed)(uint16_t);
+	void(*ButtonLongPressed)(uint16_t);
+	void(*ButtonRepeat)(uint16_t);
+	void(*ButtonRelease)(uint16_t);
+
+} button_t;
+
+// Public function
+void ButtonInitKey(button_t * Key, GPIO_TypeDef *GpioPort, uint16_t GpioPin, uint32_t TimerDebounce, uint32_t TimerLongPressed,
+					uint32_t TimerRepeat, ReverseLogicGpio_t ReverseLogic, uint16_t Number); // Initialization for state machine
+void ButtonTask(button_t *Key); //Task for working state machine
+
+//Registers functions need Callback as a pointer, type name of function without '()'
+void ButtonRegisterPressCallback(button_t *Key, void *Callback); //For first press key
+void ButtonRegisterLongPressedCallback(button_t *Key, void *Callback); //If key was long pressed
+void ButtonRegisterRepeatCallback(button_t *Key, void *Callback); //While key is long pressed
+void ButtonRegisterReleaseCallback(button_t *Key, void *Callback); //If key was released
+
+void ButtonSetDebounceTime(button_t * Key, uint32_t Miliseconds);
+void ButtonSetLongPressedTime(button_t *Key, uint32_t Miliseconds);
+void ButtonSetRepeatTime(button_t *Key, uint32_t Miliseconds);
+
+#endif /* INC_BUTTON_H_ */
